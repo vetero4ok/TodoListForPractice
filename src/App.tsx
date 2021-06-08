a6c338f712f87bb7f3b6be2adf8a10bfd9e8359d
vetero4ok <82808215+vetero4ok@users.noreply.github.com>
2021-06-08 10:27:50 +0300

    first task(todolist and props)

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import './App.css';
+import {TodoList} from './TodoList';
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+
+}
 
 function App() {
+    const tasks: Array<TaskType> = [
+        {id: 1, title: 'HTML', isDone: true},
+        {id: 2, title: 'CSS', isDone: true},
+        {id: 3, title: 'JS', isDone: false},
+    ]
+
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList
+                title={'What to learn'}
+                tasks={tasks}
+
+            />
         </div>
     );
 }
